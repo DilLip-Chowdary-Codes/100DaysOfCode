039859e4e1694b56581c308266c3e149d9d6fc28
Dillip_Chowdary Rayapati <coderdil.dillip@gmail.com>
2021-02-09 09:34:48 +0530

    Add Get_Grades Program

---
+++ Python/get_grades.py
@@ -0,0 +1,10 @@
+#10
+marks = float(input())
+if marks > 85:
+    print("A")
+elif marks > 70 and marks <= 85:
+    print("B")
+elif marks >= 60 and marks <= 70:
+    print("C")
+elif marks < 60:
+    print("F")
