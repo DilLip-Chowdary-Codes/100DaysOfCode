4d663243673e057c0fb247b692478d58567ab161
Dillip_Chowdary Rayapati <coderdil.dillip@gmail.com>
2021-02-09 09:34:48 +0530

    Add Exam_Eligibility Program

---
+++ Python/eligibility_to_write_exam.py
@@ -0,0 +1,8 @@
+#9
+attendance_percentage = int(input()[:-1])
+having_medical_report = input()
+is_eligible_for_final_exams = attendance_percentage >= 75 or having_medical_report == "Y"
+if is_eligible_for_final_exams:
+    print("Allowed to write exam")
+else:
+    print("Cannot write exam")
