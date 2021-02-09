2ba8c36e134e6e3bfc5d758d6ad7bd0242466ccf
Dillip_Chowdary Rayapati <coderdil.dillip@gmail.com>
2021-02-09 09:34:48 +0530

    Add Sum_of_digits Program (V2)

---
+++ Python/sum_of_digits_v2.py
@@ -0,0 +1,14 @@
+#15 
+
+#version_2
+num = input()
+if len(num) == 1:
+    print(int(num))
+elif len(num) == 2:
+    print(int(num[0]) + int(num[1]))
+elif len(num) == 3:
+    print(int(num[0]) + int(num[1]) + int(num[2]))
+elif len(num) == 4:
+    print(int(num[0]) + int(num[1]) + int(num[2]) + int(num[3]))
+elif len(num) == 5:
+    print(int(num[0]) + int(num[1]) + int(num[2]) + int(num[3]))
