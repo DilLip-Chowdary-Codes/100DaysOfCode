bc61b0273a98cea068abb254c610a4468d01c921
Dillip_Chowdary Rayapati <coderdil.dillip@gmail.com>
2021-02-15 12:12:00 +0530

    Add sum_of_nums_from_m_to_n Program

---
+++ Python/Loops/sum_of_nums_from_m_to_n.py
@@ -0,0 +1,17 @@
+#33
+
+
+"""
+
+__author__ = DilLip_Chowdary ❤️ Rayapati
+
+"""
+
+m = int(input())
+n = int(input())
+sum_of_nums = 0
+
+for num in range(m , n + 1):
+	sum_of_nums += num
+
+print(sum_of_nums)
