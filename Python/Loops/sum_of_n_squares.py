8739d56ec00f8a82558835ede63e43ce91560ec7
Dillip_Chowdary Rayapati <coderdil.dillip@gmail.com>
2021-02-16 12:12:00 +0530

    Add sum_of_n_squares Program

---
+++ Python/Loops/sum_of_n_squares.py
@@ -0,0 +1,17 @@
+#39
+
+
+"""
+
+__author__ = DilLip_Chowdary ❤️ Rayapati
+
+"""
+
+n = int(input())
+
+sum_of_n_squares = 0
+
+for num in range(1, n + 1):
+	sum_of_n_squares += num * num
+
+print(sum_of_n_squares)
