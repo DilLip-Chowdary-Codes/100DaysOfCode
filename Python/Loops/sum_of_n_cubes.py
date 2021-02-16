1a20c5b74b2b5316b00cd6e8bad04c0f50e2c14d
Dillip_Chowdary Rayapati <coderdil.dillip@gmail.com>
2021-02-16 12:12:00 +0530

    Add sum_of_n_cubes Program

---
+++ Python/Loops/sum_of_n_cubes.py
@@ -0,0 +1,17 @@
+#40
+
+
+"""
+
+__author__ = DilLip_Chowdary ❤️ Rayapati
+
+"""
+
+n = int(input())
+
+sum_of_n_cubes = 0
+
+for num in range(1, n + 1):
+	sum_of_n_cubes += num * num * num
+
+print(sum_of_n_cubes)
