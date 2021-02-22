9b55ee67154ef9503b9bd166e10984ff00563287
Dillip_Chowdary Rayapati <coderdil.dillip@gmail.com>
2021-02-22 12:12:00 +0530

    Add sum_and_avg_of_n_nums Program

---
+++ Python/Loops/sum_and_avg_of_n_nums.py
@@ -0,0 +1,17 @@
+#41
+
+
+"""
+
+__author__ = DilLip_Chowdary ❤️ Rayapati
+
+"""
+
+n = int(input())
+
+nums = [int(input()) for inputs in range(n)]
+
+sum_of_nums = sum(nums)
+print(sum_of_nums)
+average = sum_of_nums / n
+print(average)
