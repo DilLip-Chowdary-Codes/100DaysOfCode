51d25439e74ffffc6caf96722b7110c721e6ade4
Dillip_Chowdary Rayapati <coderdil.dillip@gmail.com>
2021-03-02 12:12:00 +0530

    Add Hallow_Square Program

---
+++ Python/Loops/Hallow_Square.py
@@ -0,0 +1,18 @@
+#53
+
+
+"""
+
+__author__ = DilLip_Chowdary ❤️ Rayapati
+
+"""
+
+n = int(input())
+
+for row in range(n):
+	for col in range(n):
+		if row == 0 or row == n-1 or col == 0 or col == n-1:
+			print("* ", end="")
+		else:
+			print("  ", end="")
+	print()
