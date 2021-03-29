24b8522b5d8970db8b5945810ad4ea9f2d329106
Dillip_Chowdary Rayapati <coderdil.dillip@gmail.com>
2021-03-29 13:03:18 +0530

    Add simple_calculator Program

---
+++ Python/simple_calculator.py
@@ -0,0 +1,23 @@
+#16
+
+
+"""
+
+__author__ = DilLip_Chowdary ❤️ Rayapati
+
+"""
+
+operation = input()
+num_1 = int(input())
+num_2 = int(input())
+if operation == "+":
+    result = num_1 + num_2
+elif operation == "-":
+    result = num_1 - num_2
+elif operation == "*":
+    result = num_1 * num_2
+elif operation == "/":
+    result = num_1 / num_2
+elif operation == "%":
+    result = num_1 % num_2
+print(result)
