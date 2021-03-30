4bccb6703d001b37d8f9bdeba44d84f2830b7d2d
Dillip_Chowdary Rayapati <coderdil.dillip@gmail.com>
2021-03-30 12:42:33 +0530

    Add print_Chars_in_a_Word Program

---
+++ Python/Loops/print_Chars_in_a_Word.py
@@ -0,0 +1,17 @@
+#25
+
+
+"""
+
+__author__ = DilLip_Chowdary ❤️ Rayapati
+
+"""
+
+a = input()
+counter = 0
+length_of_a = len(a)
+
+while counter < (length_of_a):
+    print(a[counter])
+    counter = (counter + 1)
+
